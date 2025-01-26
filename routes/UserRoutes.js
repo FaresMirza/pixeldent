7a90ff090ab11400d633634bf26897c095f5b264
FaresMirza <faresmirza93@gmail.com>
2025-01-26 21:49:34 +0300

    D

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/postUser", UserController.registerUser); // User registration
 router.get("/:user_id", UserController.getUserById); // Get user by ID
 router.put("/:user_id", UserController.updateUserById); // Update user by ID
-router.get("/getAllUsers", UserController.getAllUsers)
-router.get("/getAllAdmins", UserController.getAllAdmins)
+router.get("/users", UserController.getAllUsers)
+router.get("/admins", UserController.getAllAdmins)
 
 module.exports = router;
