3ce7da8fa4de64ee5e4d56ddc49d3e3107430f0e
FaresMirza <faresmirza93@gmail.com>
2025-01-27 14:34:27 +0300

    d

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/courses", verifyToken,verifyRole(["admin"]), CourseController.getAllCourses)
 router.post("/courses",verifyToken,verifyRole(["admin"]),  CourseController.registerCourse)
 router.get("/books",verifyToken,verifyRole(["admin"]),  BookController.getAllBooks); // Get all books
+router.get("/books/:book_id", verifyToken,verifyRole(["admin"]), BookController.getBookById)
 router.get("/courses/:course_id",verifyToken,verifyRole(["admin"]),  CourseController.getCourseById)
 router.get("/admins/:user_id",verifyToken,verifyRole(["admin"]),  UserController.getAdminById)
 router.put("/admins/:user_id",verifyToken,verifyRole(["admin"]),  UserController.updateAdminById)
