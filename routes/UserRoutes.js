f54403fe16f0452bb000327ad25cefec18df1cc9
FaresMirza <faresmirza93@gmail.com>
2025-01-27 08:56:11 +0300

    DD

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -14,6 +14,7 @@ router.get("/courses/:course_id", CourseController.getCourseById)
 router.get("/books/:book_id", BookController.getBookById); // Get a book by ID
 router.get("/users/:user_id", UserController.getUserById); // Get a user by ID
 router.put("/users/:user_id", UserController.updateUserById); // Update a user by ID
+router.put("/users/:user_id",UserController.updateAdminById)
 
 
 
