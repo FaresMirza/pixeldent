425b72fde8adfa0e1745ad77207b57be4d532ca9
FaresMirza <faresmirza93@gmail.com>
2025-01-30 23:44:42 +0300

    S

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -11,9 +11,9 @@ router.post("/courses",verifyToken,verifyRole(["admin"]),  CourseController.regi
 router.get("/books",verifyToken,verifyRole(["admin"]),  BookController.getAllBooks); // Get all books
 router.get("/books/:book_id", verifyToken,verifyRole(["admin"]), BookController.getBookById)
 router.get("/courses/:course_id",verifyToken,verifyRole(["admin"]),  CourseController.getCourseById)
+router.put("/courses/:course_id",verifyToken,verifyRole(["admin"]),  CourseController.updateCourseById)
 router.get("/admins/:user_id",verifyToken,verifyRole(["admin"]),  UserController.getAdminById)
 router.put("/admins/:user_id",verifyToken,verifyRole(["admin"]),  UserController.updateAdminById)
-router.put("/courses/:course_id",verifyToken,verifyRole(["admin"]),  CourseController.updateCourseById)
 
 
 
