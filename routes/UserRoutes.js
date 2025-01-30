e553ef7e31827fc52f3e1047b2c8a3d5ff6ac7a0
FaresMirza <faresmirza93@gmail.com>
2025-01-30 19:49:52 +0300

    d

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.get("/courses", CourseController.getAllCourses)
 router.get("/books", BookController.getAllBooks); // Get all books
+router.get("/admins",UserController.getAllAdmins)
 router.get("/usercourses", verifyToken,verifyRole(["normal"]), CourseController.getAllCoursesForUser)
 router.get("/courses/:course_id",verifyToken,verifyRole(["normal"]), CourseController.getCourseById)
 router.get("/books/:book_id",verifyToken,verifyRole(["normal"]), BookController.getBookById); // Get a book by ID
