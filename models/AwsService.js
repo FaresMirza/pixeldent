108ae08311be183ab7418d91bc7bb474cbc015ea
FaresMirza <faresmirza93@gmail.com>
2025-02-01 22:26:53 +0300

    d

--- models/AwsService.js
+++ models/AwsService.js
@@ -2,18 +2,14 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { Readable } = require("stream");
 
 const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    region: "me-south-1"
 });
 
 
 async function uploadFileToS3(fileBuffer, fileName, fileMimeType) {
     try {
         const uploadParams = {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Bucket: process.env.AWS_S3_BUCKET,
             Key: `courses/${Date.now()}-${fileName}`,
             Body: Readable.from(fileBuffer),
             ContentType: fileMimeType
@@ -21,7 +17,7 @@ async function uploadFileToS3(fileBuffer, fileName, fileMimeType) {
 
         await s3.send(new PutObjectCommand(uploadParams));
 
-        return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${uploadParams.Key}`;
+        return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION_s3}.amazonaws.com/${uploadParams.Key}`;
     } catch (error) {
         throw new Error(`Error uploading file to S3: ${error.message}`);
     }
