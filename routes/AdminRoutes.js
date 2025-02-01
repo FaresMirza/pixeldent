892055fd4af56690c4cad13bc35f81b926b44df7
FaresMirza <faresmirza93@gmail.com>
2025-02-01 17:14:58 +0300

    D

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -5,7 +5,13 @@ const CourseController = require("../controllers/CourseController")
 const { verifyToken, verifyRole } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get("/courses", verifyToken,verifyRole(["admin"]), CourseController.getAllCourses)
+const uploadFields = upload.fields([
+    { name: "course_image", maxCount: 1 }, // Upload course image
+    { name: "course_videos", maxCount: 5 }, // Upload up to 5 videos
+    { name: "lesson_*", maxCount: 1 } // Lesson videos dynamically
+]);
+
+router.get("/courses", verifyToken,verifyRole(["admin"]),uploadFields, CourseController.getAllCourses)
 router.get("/admincourses", verifyToken,verifyRole(["admin"]), CourseController.getAllCoursesForAdmin)
 router.post("/courses",verifyToken,verifyRole(["admin"]),  CourseController.registerCourse)
 router.get("/books",verifyToken,verifyRole(["admin"]),  BookController.getAllBooks); // Get all books
