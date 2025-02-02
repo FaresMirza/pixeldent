42aad84c9d5e04c8f536078a97fdc6e1d9bc057a
FaresMirza <faresmirza93@gmail.com>
2025-02-02 21:21:23 +0300

    d

--- util/fileUploadConfig.js
+++ util/fileUploadConfig.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 // ✅ إعداد S3 Client
 const s3 = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION_S3,
 
 });
 
@@ -17,7 +17,7 @@ async function uploadFileToS3(fileBuffer, fileName, fileMimeType) {
         Body: fileBuffer,
         ContentType: fileMimeType
     }));
-    return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+    return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION_S3}.amazonaws.com/${key}`;
 }
 
 module.exports = uploadFileToS3;
