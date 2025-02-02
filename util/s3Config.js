74d168cc147a1747c4e7011ef32dc688629922c9
FaresMirza <faresmirza93@gmail.com>
2025-02-02 20:53:01 +0300

    d

--- util/s3Config.js
+++ util/s3Config.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const s3 = new S3Client({
     region: process.env.AWS_REGION_S3,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
+
 });
 
 module.exports = s3;
