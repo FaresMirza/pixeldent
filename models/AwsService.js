31c7d1addb51b2b3f7c993d7995ea72f5061ed74
FaresMirza <faresmirza93@gmail.com>
2025-02-02 09:31:59 +0300

    d

--- models/AwsService.js
+++ models/AwsService.js
@@ -4,10 +4,6 @@ require("dotenv").config(); // ✅ Load environment variables
 // ✅ Initialize S3 Client
 const s3 = new S3Client({
     region: process.env.AWS_REGION_S3,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
 });
 
 /**
